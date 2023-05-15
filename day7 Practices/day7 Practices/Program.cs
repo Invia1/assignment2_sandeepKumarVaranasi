def0e774cb7854ecb49653f7b5a7eb9b7b2b03cd
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-15 16:58:13 +0530

    Add files via upload

---
+++ day7 Practices/day7 Practices/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+class personDetails
+{
+    ArrayList obj=new ArrayList();
+    public void ENtry()
+    {
+        obj.Add("sandeep");///boxing because value type convert into reference type
+        obj.Add(12000);//value type to refrence type is boxing
+        int salary = Convert.ToInt32(obj[1]);//this is known as unboxing 
+        string name = Convert.ToString(obj[0]);//reference to value type;
+        Console.WriteLine(name + " " + salary);
+
+       
+    }
+    public static void Main()
+    {
+        personDetails personDetail = new personDetails();
+        personDetail.ENtry();
+    }
+}
